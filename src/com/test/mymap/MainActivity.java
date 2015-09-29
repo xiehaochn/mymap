14ee3a2940899dd7eadc9620b49b89f126640a3f
XieHao <373485079@qq.com>
2015-09-29 20:54:02 +0800

    优化定位，增加卫星地图、交通情况

--- src/com/test/mymap/MainActivity.java
+++ src/com/test/mymap/MainActivity.java
@@ -15,6 +15,7 @@ import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 
+
 import android.app.Activity;
 
 import android.os.Bundle;
@@ -32,10 +33,12 @@ public class MainActivity extends Activity implements OnClickListener {
 	private LocationClient locationClient;
 	private MyLocationListener myListener = new MyLocationListener();
 	private ImageButton my_location;
+	private ImageButton traffic;
+	private ImageButton sattelite;
 	public LatLng my_point;
 	public int iss;
 	public int error_type;
-
+	public String my_address;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -44,8 +47,12 @@ public class MainActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_main);
 		mapView=(MapView)findViewById(R.id.bmapView);
 		baiduMap=mapView.getMap();
-		my_location=(ImageButton)findViewById(R.id.my_location);
+		my_location=(ImageButton)findViewById(R.id.show_location);
 		my_location.setOnClickListener(this);
+		traffic=(ImageButton)findViewById(R.id.traffic);
+        traffic.setOnClickListener(this);
+        sattelite=(ImageButton)findViewById(R.id.normal_satellite);
+        sattelite.setOnClickListener(this);
 		locationClient = new LocationClient(getApplicationContext());
 		locationClient.registerLocationListener( myListener );
 		initLocation();
@@ -59,10 +66,8 @@ public class MainActivity extends Activity implements OnClickListener {
 		 LocationClientOption option = new LocationClientOption();
 	     option.setLocationMode(LocationMode.Hight_Accuracy);
 	     option.setCoorType("bd09ll");
-	     int span=1000;
-	     option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
 	     option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
-	     option.setOpenGps(false);//可选，默认false,设置是否使用gps
+	     option.setOpenGps(true);//可选，默认false,设置是否使用gps
 	     option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
 	     option.setIsNeedLocationPoiList(false);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
 	    locationClient.setLocOption(option);
@@ -119,16 +124,20 @@ public class MainActivity extends Activity implements OnClickListener {
 				    .direction(100).latitude(location.getLatitude())  
 				    .longitude(location.getLongitude()).build();  
 			baiduMap.setMyLocationData(locData); 
+			
+			my_address=location.getAddrStr();
             error_type=location.getLocType();
             my_point=new LatLng(location.getLatitude(),location.getLongitude());
+            MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(my_point);  
+	        baiduMap.animateMapStatus(msu);
             if (location.getLocType() == BDLocation.TypeGpsLocation){// GPS定位结果                             
                 iss=0;
             } else if (location.getLocType() == BDLocation.TypeNetWorkLocation){// 网络定位结果                               
-                iss=0;
-            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果                              
-                iss=0;
-            } else{                          
                 iss=1;
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果                              
+                iss=2;
+            } else{                          
+                iss=3;
             }
 			
             }
@@ -139,15 +148,39 @@ public class MainActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch(v.getId()){
-		case(R.id.my_location):{
-			if(iss==0){
-				MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(my_point);  
-	            baiduMap.animateMapStatus(msu);  
-	            Toast.makeText(this, "Successed.Your location is "+my_point.latitude+"    "+my_point.longitude, Toast.LENGTH_SHORT).show();
+		case(R.id.normal_satellite):{
+			if(baiduMap.getMapType()==BaiduMap.MAP_TYPE_SATELLITE){
+			baiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
 			}else{
-				Toast.makeText(this, "Failed"+error_type, Toast.LENGTH_SHORT).show();
+				baiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
 			}
 			break;
+		}
+		case(R.id.traffic):{
+			if(baiduMap.isTrafficEnabled()){
+				baiduMap.setTrafficEnabled(false);
+			}else{
+				baiduMap.setTrafficEnabled(true);
+			}
+			break;
+		}
+		case(R.id.show_location):{
+			locationClient.requestLocation();
+			
+			if(iss==0){
+				  
+	            Toast.makeText(this, "GPS定位成功"+"\n"+"您当前的位置："+my_address, Toast.LENGTH_SHORT).show();
+			}else if(iss==1){	
+				
+				Toast.makeText(this, "网络定位成功"+"\n"+"您当前的位置："+my_address, Toast.LENGTH_SHORT).show();
+			}else if(iss==2){
+				
+				Toast.makeText(this, "离线定位成功", Toast.LENGTH_SHORT).show();
+			}else{
+				Toast.makeText(this, "定位失败"+"\n"+"error type:"+error_type, Toast.LENGTH_SHORT).show();
+			}
+				
+			
 		}
 		default:
 			break;
